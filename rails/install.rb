dd648c6ece53e5828c1764dcdeb115fe72d9fcf8
Scott Steadman <scott.steadman@geni.com>
2010-11-19 13:18:28 -0800

    Try not to sync unchanged files.

--- rails/install.rb
+++ rails/install.rb
@@ -1,8 +1,8 @@
 puts 'Running install script...'
 
 # sync/run migrations
-system "rsync -ruv #{Rails.root}/vendor/plugins/registry/db/migrate #{Rails.root}/db"
+system "rsync -ruv --size-only #{Rails.root}/vendor/plugins/registry/db/migrate #{Rails.root}/db"
 system 'rake db:migrate'
 
 # sync assets
-system "rsync -ruv #{Rails.root}/vendor/plugins/registry/public #{Rails.root}"
+system "rsync -ruv  --size-only #{Rails.root}/vendor/plugins/registry/public #{Rails.root}"
