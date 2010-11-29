47061d3c5e4e1640b32eb2c414ddb0fec8c52829
Scott Steadman <scott.steadman@geni.com>
2010-11-29 12:16:17 -0800

    Updated migration to include versions table.

--- db/migrate/20101124160135_add_notes_to_entries.rb
+++ db/migrate/20101124160135_add_notes_to_entries.rb
@@ -1,9 +1,11 @@
 class AddNotesToEntries < ActiveRecord::Migration
   def self.up
     add_column :registry_entries, :notes, :text
+    add_column :registry_entry_versions, :notes, :text
   end
 
   def self.down
     remove_column :registry_entries, :notes
+    remove_column :registry_entry_versions, :notes
   end
 end
