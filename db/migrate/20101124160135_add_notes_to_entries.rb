538be08d2b58d0ee1790ed0e637eec8be2f2fb6d
Scott Steadman <scott.steadman@geni.com>
2010-11-30 08:50:31 -0800

    registry_entry_versions isn't present yet.

--- db/migrate/20101124160135_add_notes_to_entries.rb
+++ db/migrate/20101124160135_add_notes_to_entries.rb
@@ -1,11 +1,9 @@
 class AddNotesToEntries < ActiveRecord::Migration
   def self.up
     add_column :registry_entries, :notes, :text
-    add_column :registry_entry_versions, :notes, :text
   end
 
   def self.down
     remove_column :registry_entries, :notes
-    remove_column :registry_entry_versions, :notes
   end
 end
